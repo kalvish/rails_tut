c6ec76b1d9c22e6edfc1770784ca171bc09cb5e9
kalvish <kalanvishwanath@gmail.com>
2015-08-26 17:30:16 +0800

    added email validations, index to avoid duplicates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824122049) do
+ActiveRecord::Schema.define(version: 20150826091931) do
 
   create_table "articles", force: true do |t|
     t.string   "name"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(version: 20150824122049) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
