53fba97a6215d13000331caae8638d5f219d0ebe
kalvish <kalanvishwanath@gmail.com>
2015-08-27 16:25:43 +0800

    ip change

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ load "config/recipes/rbenv"
 load "config/recipes/check"
 
 set :ssh_options, { keys: ["#{ENV['HOME']}/.ssh/fms_computer.pem"] }
-server "54.68.118.209", :web, :app, :db, primary: true
+server "54.200.129.123", :web, :app, :db, primary: true
 
 set :user, "ubuntu"
 set :application, "rails_tut"
